b28f81cc7d79b19e9eae4b241c6a4296f3f5a034
CrusarAppy <bhandariapar95@gmail.com>
2025-07-01 13:10:27 +0545

    Added Program and Arithmetic class

---
+++ ArithmeticOperation.cs
@@ -0,0 +1,13 @@
+//Write a program to add, subtract, multiply, divide, and modulo two integers.
+
+using System;
+
+class ArithmeticOperation
+{
+    public int Add(int a, int b)
+    {
+       
+        return a + b;
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello ");
+
+        int number1, number2, sum;
+        Console.WriteLine("Enter a number");
+        number1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter another number");
+        number2 = int.Parse(Console.ReadLine());
+
+        // ArithmeticOperation operation = new ArithmeticOperation();
+        // sum = operation.Add(number1, number2);
+        sum = Program.Add(number1, number2);
+        Console.WriteLine("The sum of two number is " + sum);
+    }
+     public  static int Add(int a, int b)
+    {
+       
+        return a + b;
+    }
+}
